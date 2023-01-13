7e715d1f1b050202f37c164f637aba2d42e22ec4
micaelalozano <micaelalozano.16@hotmail.com>
2023-01-13 11:14:57 -0300

    Adding new files to Git

--- src/pages/SweaterList.jsx
+++ src/pages/SweaterList.jsx
@@ -14,7 +14,7 @@ const [productos, setProductos] = useState([]);
 
   useEffect(() => {
     axios
-      .get("/api/productos")
+      .get("https://veshbymicaelalozano.onrender.com/api/productos")
       .then((res) => res.data)
       .then((productos) => {
         setProductos(productos);
